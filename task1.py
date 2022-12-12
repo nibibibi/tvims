16afc54e54e1d041f3a6fab93380a509da59b3e3
Dmitry Shaplyko <shaplykodmitry@gmail.com>
2022-12-12 23:03:13 +0300

    Pirson

--- task1.py
+++ task1.py
@@ -31,7 +31,7 @@ for i, a in enumerate(p):
     if i == 0:
         F.append(a)
     else:
-        F.append(round(F[i-1] + a, 2))
+        F.append(round(F[i-1] + a, 4))
 print("Fi:\n", F, '\n')
 
 import matplotlib.pyplot as plt
@@ -48,10 +48,10 @@ M = round(math.sqrt(n))
 h = (values[-1] - values[0]) / M
 print("h = ", h, '\n')
 # Левые границы интервалов
-Ai = [round(values[0] + h*i, 2) for i in range(M)]
+Ai = [round(values[0] + h*i, 4) for i in range(M)]
 print("Ai: ", Ai)
 # Правые границы интервалов
-Bi = [round(values[0] + h*(i+1), 2) for i in range(M)]
+Bi = [round(values[0] + h*(i+1), 4) for i in range(M)]
 print("Bi: ", Bi)
 # Количество точек  на интервалах
 mi = [0 for _ in range(M)]
@@ -68,3 +68,37 @@ print("mi: ", mi)
 # Статистическая плотность интервалов
 fi = [round(a/(h*n), 4) for a in mi]
 print(fi)
+
+fig, ax = plt.subplots()
+ax.bar(Ai, fi)
+# plt.show()
+
+intervals = [values[M*i: M*i+M] for i in range(M)]
+print("intervals: ", intervals)
+for i, interval in enumerate(intervals):
+	if i == 0:
+		Ai[0] = interval[0]
+	else:
+		if interval[0] == intervals[i-1][-1]:
+			Ai[i] = interval[0]
+			Bi[i-1] = interval[0]
+		else:
+			Ai[i] = round((interval[0] + intervals[i-1][-1]) / 2, 4)
+			Bi[i-1] = round((interval[0] + intervals[i-1][-1]) / 2, 4)
+	if i == M-1:
+		Bi[-1] = interval[-1]
+print("Ai: ", Ai)
+print("Bi: ", Bi)
+h = [round(Bi[i] - Ai[i], 4) for i in range(M)]
+print("hi: ", h)
+fi = [round(len(intervals[i]) / (h[i]*n), 4) for i in range(M)]
+print("fi: ", fi)
+
+# Матожидание
+mx = round(sum(values) / n, 4)
+print("mx: ", mx)
+# Оценка дисперсии
+SxS = round(sum([(value - mx)*(value - mx) for value in values])/(n-1), 4)
+print("S^2: ", SxS)
+S = round(math.sqrt(SxS), 4)
+print("S: ", S)
